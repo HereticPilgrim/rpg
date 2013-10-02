2a91731e70937b64e48f43cec5258a6c2b2b5685
Manuel Grauwiler <grauwilermanuel@gmail.com>
2013-10-02 23:32:42 +0200

    Added player sprite to PlayerLayer, need to figure out how to control it

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pyglet.window import key
+
 import cocos
 from cocos.scene import Scene
 from cocos.tiles import ScrollingManager
@@ -12,16 +14,17 @@ class GameScene(Scene):
 
 		self.load('test_map.tmx')
 
-		self.schedule(self.update)
+		collision_layer = self.map.get_resource('collision')
 
 		# initialise player layer
-		self.player_layer = PlayerLayer(self.scroller)
+		self.player_layer = PlayerLayer(self.scroller, collision_layer)
 		self.scroller.add(self.player_layer, z=50)
 
+		self.schedule(self.update)
 
 	def update(self, dt):
-		"""Updates the game approx. every 1/60 secs."""
-		self.player_layer.update()
+		"""Updates the game every frame, approx. 1/60 secs."""
+		self.player_layer.update(dt)
 
 
 	def load(self, map_filename):
@@ -49,9 +52,13 @@ class PlayerLayer(cocos.layer.ScrollableLayer):
 	is_event_handler = True
 	keys_pressed = set()
 
-	def __init__(self, scroller):
+	def __init__(self, scroller, collision_map):
 		super(PlayerLayer, self).__init__()
 		self.scroller = scroller
+
+		player = cocos.sprite.Sprite('img/octocat.png')
+		self.add(player, name='player')
+		# player.do(PlayerController(self.keys_pressed, collision_map, scroller))
 
 
 	def on_key_press(self, k, mod):
@@ -68,7 +75,47 @@ class PlayerLayer(cocos.layer.ScrollableLayer):
 
 
 	def update(self, dt):
+		# key_names = [key.symbol_string (k) for k in self.keys_pressed]
+
+
 		pass
+
+
+
+# class PlayerController(cocos.actions.Action, cocos.tiles.RectMapCollider):
+# 
+# 	def __init__(self, keys_pressed, collision_map, scroller):
+# 		self.keys_pressed = keys_pressed
+# 		self.collision_map = collision_map
+# 		self.scroller = scroller
+
+# 	def step(self, dt):
+# 		print self.keys_pressed
+# 		# dx, dy = self.target.velocity
+
+# 		# # using the player controls, gravity and other acceleration influences
+# 		# # update the velocity
+# 		# dx = (keyboard[key.RIGHT] - keyboard[key.LEFT]) * self.MOVE_SPEED *dt
+# 		# dy = dy + self.GRAVITY * dt
+# 		# if self.on_ground and keyboard[key.SPACE]:
+# 		# 	dy = self.JUMP_SPEED
+
+# 		# # get the player's current bounding rectangle
+# 		# last = self.target.get_rect()
+# 		# new = last.copy()
+# 		# new.x += dx
+# 		# new.y += dy * dt
+
+# 		# # run the collider
+# 		# dx, dy = self.target.velocity = self.collide_map(tilemap, last, new, dy, dx)
+# 		# self.on_ground = bool(new.y == last.y)
+
+# 		# # player position is anchored in the center of the image rect
+# 		# self.target.position = new.center
+
+# 		# # move the scrolling view to center on the player
+# 		# scroller.set_focus(*new.center)
+
 
 
 cocos.director.director.init(width=800, height=600, caption='Testing', do_not_scale=True)
