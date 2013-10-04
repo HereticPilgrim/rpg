b7b9178325a186ee5ec965c4f1933d4f84100dc1
Manuel Grauwiler <grauwilermanuel@gmail.com>
2013-10-04 21:21:47 +0200

    Removed load() from GameScene, since we generate maps online

--- main.py
+++ main.py
@@ -27,26 +27,7 @@ class GameScene(Scene):
 	def update(self, dt):
 		"""Updates the game every frame, approx. 1/60 secs."""
 		pass
-
-
-	def load(self, map_filename):
-		"""Loads the specified map and makes the collision layer collidable."""
-		# load the tiled map in TMX format (www.mapeditor.org)
-		self.map = cocos.tiles.load_tmx(map_filename)
-
-		# find collision layer
-		collision_layer = self.map.get_resource('collision')
-		# collision_layer['visible'] = False
-
-		# make all cells in layer 'collision' collidable
-		for row in collision_layer.cells:
-			for cell in row:
-				# set collision properties
-				cell['top'] = cell['bottom'] = cell['right'] = cell['left'] = True
-
-		for name, layer in self.map.findall(cocos.tiles.RectMapLayer):
-			self.scroller.add(layer, name=name)
-
+	
 
 	def generate_dungeon(self, width, height):
 		self.dungeon_map = self.dungeon_generator.generate(width, height)
